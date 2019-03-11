43c123b91d98873d46a1b15bed325f54b71cb1e2
Anushree C <anushreec2@gmail.com>
2019-03-11 08:58:55 -0700

    testing letter file and class

---
+++ src/main/Letter.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.HashMap;
+
+public class Letter {
+	int x, y;
+	int value;
+	HashMap <String, Integer> values = new HashMap<String, Integer>();
+	
+	public Letter(int x, int y) {
+		this.x = x;
+		this.y = y;
+		
+	}
+
+}
