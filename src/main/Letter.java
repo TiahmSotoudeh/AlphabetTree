9a1fb17c9938edadb37e3db3e4aecb86a1f7cee9
austin-li <austinmingli@gmail.com>
2019-03-11 09:30:23 -0700

    added Tree and moved stuff from Letter to Tree

--- src/main/Letter.java
+++ src/main/Letter.java
@@ -1,39 +1,16 @@
 package main;
 
-import java.util.HashMap;
-import java.util.Scanner;
-
 public class Letter {
-	int x, y;
-	String c;
-	int value;
-	HashMap <String, Integer> values = new HashMap<String, Integer>();
-
+	private int x, y;
+	private char c;
 	
-	public Letter(String c, int x, int y) {
+	public Letter(char c, int x, int y) {
 		this.c = c;
 		this.x = x;
 		this.y = y;
-		this.value = values.get(c);
-
 	}
 	
-	public void makeValuesMap() {
-		Scanner scan = new Scanner("letter_values.txt");
-		while (scan.hasNext()) {
-		values.put(scan.next(), scan.nextInt());
-		}
-		scan.close();
+	public char getChar() {
+		return c;
 	}
-	
-	public int calcValues(String word) {
-		int total = 0;
-		for (int i = 0; i < word.length()-1; i++) {
-			total += values.get(word.substring(i, i+1));
-		}
-		return total;
-	}
-	
-	
-
 }
--- src/main/Tree.java
+++ src/main/Tree.java
@@ -1,5 +1,41 @@
 package main;
 
-public class Tree {
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Tree {
+	
+	private Stack<Letter> letters;
+	private int wordValue;
+	
+	private HashMap <Character, Integer> values = new HashMap<Character, Integer>();
+	
+	public Tree() {
+		letters = new Stack<>();
+		wordValue = 0;
+		Scanner scan = new Scanner("letter_values.txt");
+		while (scan.hasNext()) {
+			values.put(scan.next().charAt(0), scan.nextInt());
+		}
+		scan.close();
+	}
+
+	public void push(Letter l) {
+		letters.push(l);
+	}
+	
+	public void pop() {
+		if (!letters.isEmpty()) {
+			letters.pop();
+		}
+	}
+	
+	public int calculateValue() {
+		int value = 0;
+		for (Letter l : letters) {
+			value += values.get(l.getChar());
+		}
+		return value;
+	}
 }
