8885de9f3c8f9c7a5abcff915024aef3af297b75
Anushree C <anushreec2@gmail.com>
2019-03-12 08:33:03 -0700

    tree updates

--- src/main/Tree.java
+++ src/main/Tree.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -11,14 +12,15 @@ public class Tree {
 	
 	private Stack<Letter> letters;
 	private int wordValue;
-	
+	private int treeX, treeY;
 	private HashMap <Character, Integer> values = new HashMap<Character, Integer>();
 	private HashSet<String> dictionary = new HashSet<>();
 	
 	public Tree() {
 		letters = new Stack<>();
 		wordValue = 0;
-		
+		treeX = 50;
+		treeY = 50;
 		Scanner scan = null;
 		try {
 			scan = new Scanner(new File("src/resources/letter_values.txt"));
@@ -51,6 +53,14 @@ public class Tree {
 		if (!letters.isEmpty()) {
 			letters.pop();
 		}
+	}
+	public Letter generateLetter(){
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		int index = (int) Math.random()*26;
+		char c = alphabet.charAt(index);
+		int x = (int) Math.random()*51;
+		int y = (int) Math.random()*51;
+		return new Letter(c, x, y);
 	}
 	
 	public int calculateValue() {
