dab736b4d905d36ae7c467e5f87c91f51f5f52f7
Anushree C <anushreec2@gmail.com>
2019-03-12 08:37:40 -0700

    falling letters

--- src/main/Letter.java
+++ src/main/Letter.java
@@ -13,4 +13,24 @@ public class Letter {
 	public char getChar() {
 		return c;
 	}
+	
+	public void falling(Letter l) {
+		l.setY(l.getY()+5);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 }
